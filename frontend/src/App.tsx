a8be0fe4dd22fb83f840ade6e2c9ed4c9c5e3a30
Ravikant <ravikantyadav3243@gmail.com>
2024-12-21 18:19:15 +0530

    Started the CodeQuest Porject with setting up the frontend and backend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import Problems from "./components/Problems";
+
+function App() {
+
+   return <>
+      <BrowserRouter>
+
+         <Routes>
+
+            ONline
+
+         </Routes>
+      </BrowserRouter>
+   </>;
+}
+
+export default App;
